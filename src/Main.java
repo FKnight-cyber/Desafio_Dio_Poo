8e778ce190590c27e0dae6323c994dcc99ec930c
ryannicholas <ryannicholas.vieira@gmail.com>
2023-04-24 10:12:23 -0300

    add Mentoria and Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(140);
+
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Java");
+        mentoria1.setDescricao("Descrição do curso Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
